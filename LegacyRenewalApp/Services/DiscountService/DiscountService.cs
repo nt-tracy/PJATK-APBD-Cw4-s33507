79648283aac253f093c7e4a4dec017a9f9c49bae
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-04-09 19:50:01 +0200

    Renamed DiscountResult -> CalculateResult, added it into DiscountService

--- LegacyRenewalApp/Services/DiscountService/DiscountService.cs
+++ LegacyRenewalApp/Services/DiscountService/DiscountService.cs
@@ -4,39 +4,39 @@ namespace LegacyRenewalApp;
 
 public class DiscountService : IDiscountService
 {
-    public DiscountResult GetDiscountFromYearsWithCompany(int yearsWithCompany, decimal baseAmount)
+    public CalculationResult GetDiscountFromYearsWithCompany(int yearsWithCompany, decimal baseAmount)
         => yearsWithCompany switch
         {
-            >= 5 => new DiscountResult(baseAmount * 0.07m, "long-term loyalty discount; "),
-            >= 2 => new DiscountResult(baseAmount * 0.03m, "basic loyalty discount; "),
-            _ => new DiscountResult(0, string.Empty)
+            >= 5 => new CalculationResult(baseAmount * 0.07m, "long-term loyalty discount; "),
+            >= 2 => new CalculationResult(baseAmount * 0.03m, "basic loyalty discount; "),
+            _ => new CalculationResult(0, string.Empty)
         };
 
 
-    public DiscountResult GetDiscountFromSeatCount(int seatCount, decimal baseAmount)
+    public CalculationResult GetDiscountFromSeatCount(int seatCount, decimal baseAmount)
         => seatCount switch
         {
-            >= 50 => new DiscountResult(baseAmount * 0.12m, "large team discount; "),
-            >= 20 => new DiscountResult(baseAmount * 0.08m, "medium team discount; "),
-            >= 10 => new DiscountResult(baseAmount * 0.04m, "small team discount; "),
-            _ => new DiscountResult(0, string.Empty)
+            >= 50 => new CalculationResult(baseAmount * 0.12m, "large team discount; "),
+            >= 20 => new CalculationResult(baseAmount * 0.08m, "medium team discount; "),
+            >= 10 => new CalculationResult(baseAmount * 0.04m, "small team discount; "),
+            _ => new CalculationResult(0, string.Empty)
         };
 
-    public DiscountResult GetDiscountFromCustomerSegment(string segment, bool isEducationEligible, decimal baseAmount)
+    public CalculationResult GetDiscountFromCustomerSegment(string segment, bool isEducationEligible, decimal baseAmount)
     {
         if (isEducationEligible)
         {
             return segment switch
             {
-                "Silver" => new DiscountResult(baseAmount * 0.05m, "silver discount; "),
-                "Gold" => new DiscountResult(baseAmount * 0.10m, "gold discount; "),
-                "Platinum" => new DiscountResult(baseAmount * 0.15m, "platinum discount; "),
-                "Education" => new DiscountResult(baseAmount * 0.20m, "education discount; "),
-                _ => new DiscountResult(0, string.Empty)
+                "Silver" => new CalculationResult(baseAmount * 0.05m, "silver discount; "),
+                "Gold" => new CalculationResult(baseAmount * 0.10m, "gold discount; "),
+                "Platinum" => new CalculationResult(baseAmount * 0.15m, "platinum discount; "),
+                "Education" => new CalculationResult(baseAmount * 0.20m, "education discount; "),
+                _ => new CalculationResult(0, string.Empty)
             };
 
         }
 
-        return new DiscountResult( 0.0m, String.Empty);
+        return new CalculationResult( 0.0m, String.Empty);
     }
 }
--- LegacyRenewalApp/Services/DiscountService/IDiscountService.cs
+++ LegacyRenewalApp/Services/DiscountService/IDiscountService.cs
@@ -2,9 +2,9 @@
 
 public interface IDiscountService
 {
-    public DiscountResult GetDiscountFromYearsWithCompany(int yearsWithCompany, decimal baseAmount);
-    public DiscountResult GetDiscountFromSeatCount(int seatCount, decimal baseAmount);
-    public DiscountResult GetDiscountFromCustomerSegment(string segment, bool isEducationEligible ,decimal baseAmount);
+    public CalculationResult GetDiscountFromYearsWithCompany(int yearsWithCompany, decimal baseAmount);
+    public CalculationResult GetDiscountFromSeatCount(int seatCount, decimal baseAmount);
+    public CalculationResult GetDiscountFromCustomerSegment(string segment, bool isEducationEligible ,decimal baseAmount);
 
 
 }
