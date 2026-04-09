0551cf20c3192023a1a6767870e6e3f7ce12755a
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-04-09 20:43:33 +0200

    Added ValidationsService

---
+++ LegacyRenewalApp/Services/ValidationsService/IValidationService.cs
@@ -0,0 +1,7 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IValidationService
+{
+    void ValidateRenewalInputs(int customerId, string planCode, int seatCount, string paymentMethod);
+    void ValidateBusinessRules(Customer customer);
+}
---
+++ LegacyRenewalApp/Services/ValidationsService/ValidationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LegacyRenewalApp;
+using LegacyRenewalApp.Exceptions;
+
+public class ValidationService : IValidationService
+{
+    public void ValidateRenewalInputs(int customerId, string planCode, int seatCount, string paymentMethod)
+    {
+        if (customerId <= 0) throw new NegativeCustomerIdException();
+        if (string.IsNullOrWhiteSpace(planCode)) throw new InvalidPlanCodeException();
+        if (seatCount <= 0) throw new NegativeSeatCountException();
+        if (string.IsNullOrWhiteSpace(paymentMethod)) throw new InvalidPaymentMethodException();
+    }
+
+    public void ValidateBusinessRules(Customer customer)
+    {
+        if (customer == null) throw new ArgumentNullException(nameof(customer));
+        if (!customer.IsActive) throw new InactiveCustomerException();
+    }
+}
