e7463f9fafbd52a5b4e9b4393711d95dbcdc64af
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-04-09 19:37:55 +0200

    Implemented DiscountService

---
+++ LegacyRenewalApp/Services/DiscountService/IDiscountService.cs
@@ -0,0 +1,10 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IDiscountService
+{
+    public DiscountResult GetDiscountFromYearsWithCompany(int yearsWithCompany, decimal baseAmount);
+    public DiscountResult GetDiscountFromSeatCount(int seatCount, decimal baseAmount);
+    public DiscountResult GetDiscountFromCustomerSegment(string segment, bool isEducationEligible ,decimal baseAmount);
+
+
+}
