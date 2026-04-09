0a4ca9562accf56b5f4ed24160040da0cb8f5f20
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-04-09 20:42:48 +0200

    Added BillingService

---
+++ LegacyRenewalApp/Services/BillingService/BillingService.cs
@@ -0,0 +1,14 @@
+﻿namespace LegacyRenewalApp;
+
+public class BillingService : IBillingService {
+    public void Save(RenewalInvoice invoice) => LegacyBillingGateway.SaveInvoice(invoice);
+    public void NotifyCustomer(Customer customer, RenewalInvoice invoice, string planCode) {
+        
+        if (string.IsNullOrWhiteSpace(customer.Email)) return;
+        
+        string subject = "Subscription renewal invoice";
+        string body = $"Hello {customer.FullName}, your renewal for plan {planCode} has been prepared. Final amount: {invoice.FinalAmount:F2}.";
+        
+        LegacyBillingGateway.SendEmail(customer.Email, subject, body);
+    }
+}
---
+++ LegacyRenewalApp/Services/BillingService/IBillingService.cs
@@ -0,0 +1,6 @@
+﻿namespace LegacyRenewalApp;
+
+public interface IBillingService {
+    void Save(RenewalInvoice invoice);
+    void NotifyCustomer(Customer customer, RenewalInvoice invoice, string planCode);
+}
