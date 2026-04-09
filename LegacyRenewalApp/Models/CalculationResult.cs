57f32c74d8c56169d7f8c214b6791ad6d0025327
Natalia Tracewicz <s33507@pjwstk.edu.pl>
2026-04-09 19:50:45 +0200

    Added CalculationResult

--- LegacyRenewalApp/Models/CalculationResult.cs
+++ LegacyRenewalApp/Models/CalculationResult.cs
@@ -1,6 +1,6 @@
 ﻿namespace LegacyRenewalApp;
 
-public class DiscountResult(decimal discount, string note)
+public class CalculationResult(decimal discount, string note)
 {
     public decimal Discount { get; set; } = discount;
     public string Note { get; set; } = note;
